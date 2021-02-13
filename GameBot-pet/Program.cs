4b40baa9c66a7955694993fc8e1d7cb4453cc720
Tony Liu <phyberosis@gmail.com>
2021-02-12 21:15:00 -0800

    init

---
+++ GameBot-pet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBot_pet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
